7f12c1fdd80a8b5027d444e95f140c521c5b46c1
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-14 22:20:14 +0200

    Update populateShop.js

--- js/populateShop.js
+++ js/populateShop.js
@@ -12,7 +12,7 @@ $(window).ready(function(e) {
 					
                     list.forEach(function(obj, index) {
 												
-                    var divImage = '<img src="' + obj.img_url + '" class="figure-img img-fluid" alt="Product Img"></div>';
+                    var divImage = '<img src="" class="figure-img img-fluid" alt="Product Img"></div>';
 					var divQVbutton = '<div class="quick-view-btn"><div class="compare-btn"><button id="car.' + obj.id + '" type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target=".bd-example-modal-lg-product-1"><i class="fa fa-eye" aria-hidden="true"></i> Quick view</button></div></div>';
 					var divPrice = '<strong class="active-color"><u>'+ obj.price + '</u></strong></div></div>';
 					var divDesc = '<div class="col-12"><div class="content-excerpt"><h6 class="product-title">'+ obj.brand +' '+ obj.model +' <br>'+ obj.year +'</h6><p class="product-content">'+ obj.descr +'</p></div></div>';

9333d0b1f93d5fde46f85d38363f3ef679278327
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-14 15:25:53 +0200

    Update cars_submit.php

--- cars_submit.php
+++ cars_submit.php
@@ -1,10 +1,11 @@
 <?php
-echo '<h3 style="text-align:center;">A very detailed error message ( ͡° ͜ʖ ͡°)</h3>';
+//echo '<h3 style="text-align:center;">A very detailed error message ( ͡° ͜ʖ ͡°)</h3>';
 $host = "localhost";
 $db_name = "cars";
 $username = "webuser";
 $password = "webuserpass1";
 $connection = null;
+
 try{
 $connection = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
 $connection->exec("set names utf8");
@@ -12,7 +13,5 @@ $connection->exec("set names utf8");
 echo "Connection error: " . $exception->getMessage();
 }
 
-function saveData($brand, $model, $engine_type, $engine_capacity, $year, $price, $color, $gear_box, $descr){
-global $connection;
-$query = "INSERT INTO cars(brand, model, engine_type, engine_capacity, year, price, color, gear_box, descr) VALUES( :brand, :model, :engine_type, :engine_capacity, :year, :price, :color, :gear_box, :descr);";
+$query = "INSERT INTO cars (brand, model, engine_type, engine_capacity, year, price, color, gear_box, descr) VALUES( '$brand', '$model', '$engine_type', '$engine_capacity', '$year', '$price', '$color', '$gear_box', '$descr')";
 ?>

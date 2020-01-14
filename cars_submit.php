bacab2a902bd674715699f2b7f2fd9531cb3b72b
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-14 15:42:02 +0200

    Update cars_submit.php

--- cars_submit.php
+++ cars_submit.php
@@ -8,18 +8,39 @@ $connection = null;
 
 try{
 $connection = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
-$connection->exec("set names utf8");
-}catch(PDOException $exception){
-echo "Connection error: " . $exception->getMessage();
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
 }
 
-$brand = mysqli_real_escpae_string($connection,$_REQUEST['brand']);
-
-$query = "INSERT INTO cars (brand, model, engine_type, engine_capacity, year, price, color, gear_box, descr) VALUES( '$brand', '$model', '$engine_type', '$engine_capacity', '$year', '$price', '$color', '$gear_box', '$descr')";
-
-if(mysqli_query($connection, $query)){
-    echo "Car records added successfully.";
-} else{
-    echo "ERROR: Could not able to execute $query. " . mysqli_error($connection);
+try {
+    $query = "INSERT INTO cars (brand, model, engine_type, engine_capacity, year, price, color, gear_box, descr) VALUES( :brand, :model, :engine_type, :engine_capacity, :year, :price, :color, :gear_box, :descr)";
+    $stmt = $connection->prepare($query);
+    
+    $stmt->bindParam(':brand',$_REQUEST['brand']);
+    $stmt->bindParam(':model',$_REQUEST['model']);
+    $stmt->bindParam(':engine_type',$_REQUEST['engine_type']);
+    $stmt->bindParam(':engine_capacity',$_REQUEST['engine_capacity'];
+    $stmt->bindParam(':year',$_REQUEST['year']);
+    $stmt->bindParam(':price',$_REQUEST['price']);
+    $stmt->bindParam(':color',$_REQUEST['color']);
+    $stmt->bindParam(':gear_box',$_REQUEST['gear_box']);
+    $stmt->bindParam(':descr',$_REQUEST['descr']);
+    
+    $stmt->execute();
+    echo "Records inserted successfully.";
+} catch(PDOException $e){
+    die("ERROR: Could not able to execute $query. " . $e->getMessage());
 }
+unset($connection);
+                     
+//$brand = mysqli_real_escpae_string($connection,$_REQUEST['brand']);
+
+//query = "INSERT INTO cars (brand, model, engine_type, engine_capacity, year, price, color, gear_box, descr) VALUES( :brand, :model, :engine_type, :engine_capacity, :year, :price, :color, :gear_box, :descr)";
+
+//if(mysqli_query($connection, $query)){
+ //   echo "Car records added successfully.";
+//} else{
+//    echo "ERROR: Could not able to execute $query. " . mysqli_error($connection);
+//}
 ?>

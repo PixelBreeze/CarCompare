c9d3a052bbfb187f4ac286ebd36138171438e34f
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-14 16:05:04 +0200

    Update cars_submit.php

--- cars_submit.php
+++ cars_submit.php
@@ -17,6 +17,7 @@ $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
     $query = "INSERT INTO cars (brand, model, engine_type, engine_capacity, year, price, color, gear_box, descr) VALUES( :brand, :model, :engine_type, :engine_capacity, :year, :price, :color, :gear_box, :descr)";
     $stmt = $connection->prepare($query);
+echo "This is query $query ";
 /*
     $stmt->bindParam(':brand',$_REQUEST['brand']);
     $stmt->bindParam(':model',$_REQUEST['model']);

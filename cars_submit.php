86d40f7b6d74f668c654763a8cb831285a557d7d
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-14 15:04:35 +0200

    Update cars_submit.php

--- cars_submit.php
+++ cars_submit.php
@@ -1,3 +1,14 @@
 <?php
 echo '<h3 style="text-align:center;">A very detailed error message ( ͡° ͜ʖ ͡°)</h3>';
+$host = "localhost";
+$db_name = "cars";
+$username = "root";
+$password = "no4pantsu0life";
+$connection = null;
+try{
+$connection = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+$connection->exec("set names utf8");
+}catch(PDOException $exception){
+echo "Connection error: " . $exception->getMessage();
+}
 ?>

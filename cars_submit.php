da13aba68f3dee36e0557987cd6ea21b24657be9
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-14 15:16:19 +0200

    Update cars_submit.php

--- cars_submit.php
+++ cars_submit.php
@@ -11,8 +11,7 @@ $connection->exec("set names utf8");
 }catch(PDOException $exception){
 echo "Connection error: " . $exception->getMessage();
 }
-?>
-
 function saveData($brand, $model, $engine_type,$engine_capacity,$year,$price,$color,$gear_box,$descr){
 global $connection;
 $query = "INSERT INTO cars(brand, model, engine_type, engine_capacity, year, price, color, gear_box, descr) VALUES( :brand, :model, :engine_type, :engine_capacity, :year, :price, :color, :gear_box, :descr)";
+?>

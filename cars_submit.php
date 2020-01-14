a8e26bab435de3ee61880ab5ced43bb1310a4484
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-14 15:13:56 +0200

    Update cars_submit.php

--- cars_submit.php
+++ cars_submit.php
@@ -2,8 +2,8 @@
 echo '<h3 style="text-align:center;">A very detailed error message ( ͡° ͜ʖ ͡°)</h3>';
 $host = "localhost";
 $db_name = "cars";
-$username = "root";
-$password = "no4pantsu0life";
+$username = "webuser";
+$password = "webuserpass1";
 $connection = null;
 try{
 $connection = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);

7f0695085b6562ff0f3cfb86bb786ff72b447553
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-15 16:15:57 +0200

    Update carcompare.js

--- js/carcompare.js
+++ js/carcompare.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function compareCar(event){
   var domElement = event.target;//$(event.target);
   var carid = domElement.id;
-  var carTitle = domElement.parentElement.parentElement.previousSibling;
+  var carTitle = domElement.parentElement.parentElement.previousSibling.childNodes[0].childNodes[1].innerHTML;
   var carCount = 0//getCookie('carCount');
   if(carCount < 3) {
     carCount++;

fb7410ee447600af495bfb349fc22d46a9aaadc1
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-15 21:57:39 +0200

    Update carcompare.js

--- js/carcompare.js
+++ js/carcompare.js
@@ -75,7 +75,7 @@ function compareCar(event){
       setCookie('carInfo3',carInfo,7);
       addCarToList(carInfo);
     }
-    arCount++;
+    carCount++;
     setCookie('carCount',carCount,7);
     console.log(carCount);
     document.getElementsByClassName("fa-balance-scale")[0].parentElement.nextSibling.nextSibling.innerHTML = carCount;

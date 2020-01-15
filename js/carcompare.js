6877845d584b5a7d7b60e6c5737aeeb90cf97a2a
PixelBreeze <thepixelbreeze@gmail.com>
2020-01-15 15:08:26 +0200

    Update carcompare.js

--- js/carcompare.js
+++ js/carcompare.js
@@ -1,5 +1,36 @@
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "0";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var carCount = getCookie('carCount');
+    console.log(carCount);
+}, false);
+
 function compareCar(event){
   var domElement = $(event.target);
   var carid = domElement.attr('id');
-  console.log(carid);
+  var carCount = getCookie('carCount');
+  console.log(carCount);
+  
+  //setCookie('carCount',i,7);
 }
